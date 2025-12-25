ae78484d7edc232d2325bf45e00872602956b930
AlbertUmoren20 <albertumoren2004@gmail.com>
2025-12-25 20:54:55 +0100

    Explanation of Annotations

--- Annotations.java
+++ Annotations.java
@@ -1,10 +1,20 @@
+// What are Annotations 
+// Annotation  annotations are metadata that provide additional information about program e
+// lements such as classes, methods, variables, or parameters. 
+// They do not directly affect the program's execution but can influence how the compiler,
+//  runtime, or other tools process the code. Annotations are widely used for documentation,
+//  validation, configuration, and enhancing code readability.
+
+//Annotations starts with a "@" symbol and can be applied to various program elements
+//E.g @Override, @Depracated are commonly used to give specific instructions to the compiler
+
+
 public class Annotations {
     class A{
         void ShowWhatIsInThePrintForThiss(){
             System.out.println("In A Show");
         }
     }
-    
     class B extends A{
         void ShowWhatIsInThePrintForThis(){
             System.out.println("In B Show");
